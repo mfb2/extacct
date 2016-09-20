547a7dd6858095ebfd497565311373f9b5cf69de
Manny Batule <manny@puppet.com>
2016-09-20 13:40:25 -0700

    MessageBuilder Refactor
    Refactored MessageBuilder for ease of testing and future GenServer
    implementation

--- lib/extacct.ex
+++ lib/extacct.ex
@@ -5,29 +5,35 @@ defmodule Extacct do
   alias Extacct.FunctionBuilder
   alias Extacct.ResponseHandler
 
-  @control_id "testFunctionId"
-  @all_fields "*"
-
-  def read(object, keys),        do: read(object, @all_fields, keys)
+  def read(object, keys) do
+    MessageBuilder.read(object, keys)
+    |> process_request
+  end
   def read(object, fields, keys) do
-    FunctionBuilder.read(object, fields, keys, @control_id)
+    MessageBuilder.read(object, fields, keys)
     |> process_request
   end
 
-  def read_by_name(object, keys),        do: read_by_name(object, @all_fields, keys)
+  def read_by_name(object, keys) do
+    MessageBuilder.read_by_name(object, keys)
+    |> process_request
+  end
   def read_by_name(object, fields, keys) do
-    FunctionBuilder.read_by_name(object, fields, keys, @control_id)
+    MessageBuilder.read_by_name(object, fields, keys)
     |> process_request
   end
 
-  def read_by_query(object, query),        do: read_by_query(object, @all_fields, query)
+  def read_by_query(object, query) do
+    MessageBuilder.read_by_query(object, query)
+    |> process_request
+  end
   def read_by_query(object, fields, query) do
-    FunctionBuilder.read_by_query(object, fields, query, @control_id)
+    MessageBuilder.read_by_query(object, fields, query)
     |> process_request
   end
 
   def read_report(report_name) do
-    FunctionBuilder.read_report(report_name, false, @control_id)
+    MessageBuilder.read_report(report_name)
     |> process_request
   end
 
@@ -42,14 +48,12 @@ defmodule Extacct do
 
   """
   def read_more(method, identifier) do
-    FunctionBuilder.read_more(method, identifier, @control_id)
+    MessageBuilder.read_more(method, identifier)
     |> process_request
   end
 
-  defp process_request(functions) do
-    functions
-    |> MessageBuilder.request(@control_id)
-    |> XmlBuilder.generate
+  defp process_request(request) do
+    request
     |> gateway.send_request
     |> ResponseHandler.parse
   end
--- lib/extacct/builder/message_builder.ex
+++ lib/extacct/builder/message_builder.ex
@@ -1,11 +1,49 @@
 defmodule Extacct.MessageBuilder do
   import Extacct.EnvironmentHelper
 
-  def request(functions, control_id) do
+  alias Extacct.FunctionBuilder
+
+  @control_id "testFunctionId"
+  @all_fields "*"
+
+  def read(object, keys),        do: read(object, @all_fields, keys)
+  def read(object, fields, keys) do
+    FunctionBuilder.read(object, fields, keys, @control_id)
+    |> build_xml_request(@control_id)
+  end
+
+  def read_by_name(object, keys),        do: read_by_name(object, @all_fields, keys)
+  def read_by_name(object, fields, keys) do
+    FunctionBuilder.read_by_name(object, fields, keys, @control_id)
+    |> build_xml_request(@control_id)
+  end
+
+  def read_by_query(object, query),        do: read_by_query(object, @all_fields, query)
+  def read_by_query(object, fields, query) do
+    FunctionBuilder.read_by_query(object, fields, query, @control_id)
+    |> build_xml_request(@control_id)
+  end
+
+  def read_report(report_name) do
+    FunctionBuilder.read_report(report_name, false, @control_id)
+    |> build_xml_request(@control_id)
+  end
+
+  def read_more(method, identifier) do
+    FunctionBuilder.read_more(method, identifier, @control_id)
+    |> build_xml_request(@control_id)
+  end
+
+  def build_xml_request(functions, control_id) do
+    functions
+    |> request(control_id)
+    |> XmlBuilder.generate
+  end
+
+  def request(functions, control_id), do:
     {
       :request, no_params, [control(control_id)] ++ [operation(functions)]
     }
-  end
 
   def control(control_id), do:
     {
