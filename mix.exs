402288efe8d9a5200578c25066037c503d133940
Manny Batule <manny@puppet.com>
2017-04-22 11:51:45 -0700

    Version Upgrade
    Updating dependency versions and bumping project version

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule Extacct.Mixfile do
 
   def project do
     [app: :extacct,
-     version: "0.2.0",
+     version: "0.3.0",
      elixir: "~> 1.3",
      elixirc_paths: elixirc_paths(Mix.env),
      build_embedded: Mix.env == :prod,
@@ -18,10 +18,9 @@ defmodule Extacct.Mixfile do
   defp deps do
     [
       {:erlsom, "~> 1.4"},
-      {:intacct_dtd, github: "intacct/intacct_dtd", compile: false, app: false},
-      {:poison, " ~> 2.0"},
-      {:httpoison, " ~> 0.9"},
-      {:xml_builder, "~> 0.0.8"},
+      {:poison, " ~> 3.1"},
+      {:httpoison, " ~> 0.11"},
+      {:xml_builder, "~> 0.0.9"},
       {:sweet_xml, "~> 0.6"},
     ]
   end
